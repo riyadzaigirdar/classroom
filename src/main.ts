723a747aa850d0872afbcb55e4cbdd84cb845146
Abdus Salam Zaigirdar Arogga <zaigirdar.arogga@gmail.com>
2023-05-24 15:20:10 +0600

    oke gone 4

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ValidationError } from 'class-validator';
 import { AppModule } from './app.module';
-console.log('ok gone 3')
+console.log('ok gone 4')
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
