15174e1dbce981b572a738904d2c44192e8a1de4
riyad <zaigirdar@loopfreight.io>
2023-05-27 11:35:07 +0600

    ok 7 gone 2

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ValidationError } from 'class-validator';
 import { AppModule } from './app.module';
-console.log('ok gone 7')
+console.log('ok gone 7');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
